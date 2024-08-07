231cf1514cb1f8aec474107660b38b59fc0f60b6
jaydenvii <jaydenyz456@gmail.com>
2024-08-07 19:27:44 -0400

    Path now functional, full countries.json

--- client/src/components/FlagGrid.jsx
+++ client/src/components/FlagGrid.jsx
@@ -1,55 +1,118 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import countryData from "../countries.json";
 
 const FlagGrid = () => {
-  const firstCountry = "AF";
-  const firstCountryClicked = false;
-  const lastCountry = "";
-  const [allCountries, setAllCountries] = useState([]);
+  const firstCountry = "PRT";
+  const [firstCountryClicked, setFirstCountryClicked] = useState(false);
+  const [currCountry, setCurrCountry] = useState("");
+  const [prevCountry, setPrevCountry] = useState("");
+  const lastCountry = "BLR";
+  const [allCountries, setAllCountries] = useState({});
   const [gridCountries, setGridCountries] = useState([]);
-  const gameFinished = false;
+  const [gameFinished, setGameFinished] = useState(false);
+  const [clickedFlagColors, setClickedFlagColors] = useState({});
   const [loading, setLoading] = useState(true);
 
-  // Getting all countries' data
+  // Setting all countries' data
   useEffect(() => {
-    const fetchAllCountries = async () => {
-      const apiUrl = "/api/countries";
+    setAllCountries(countryData);
 
-      try {
-        const res = await fetch(apiUrl);
-        const data = await res.json();
-        setAllCountries(data);
-      } catch (error) {
-        console.log("Error fetching countries' data", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchAllCountries();
+    setGridCountries([
+      "AFG",
+      "AGO",
+      "ALB",
+      "PRT",
+      "ARE",
+      "BGR",
+      "ARG",
+      "FRA",
+      "AND",
+      "ESP",
+      "ARM",
+      "BHR",
+      "CAN",
+      "BEL",
+      "ATG",
+      "AUS",
+      "BHS",
+      "AUT",
+      "AZE",
+      "NLD",
+      "DEU",
+      "CZE",
+      "SVK",
+      "BIH",
+      "BDI",
+      "BEN",
+      "BFA",
+      "BGD",
+      "UKR",
+      "MNG",
+      "COL",
+      "CUBA",
+      "CRI",
+      "CPV",
+      "BLR",
+      "MOZ",
+    ]);
   }, []);
 
-  const flagClick = (country) => {
-    if (!firstCountryClicked && country.id === firstCountry) {
-      console.log("First country clicked");
-    } else if (country.id === lastCountry) {
-      console.log("Game finished");
-      gameFinished = true;
+  // Handles game progression by clicking on flags
+  const flagClick = (id) => {
+    // First correct country
+    if (!firstCountryClicked && id === firstCountry) {
+      setFirstCountryClicked(true);
+      setCurrCountry(firstCountry);
+
+      changeFlagColor(id, "green");
     }
-    console.log(country.name);
+    // Subsequent correct countries
+    else if (
+      firstCountryClicked &&
+      allCountries[currCountry].borders.includes(id) &&
+      id != prevCountry
+    ) {
+      setPrevCountry(currCountry);
+      setCurrCountry(id);
+
+      changeFlagColor(id, "green");
+
+      if (id === lastCountry) {
+        setGameFinished(true);
+      }
+    }
+    // Incorrect country
+    else {
+      changeFlagColor(id, "red");
+    }
+  };
+
+  // Changes the flags' colours to show a correct/incorrect guess
+  const changeFlagColor = (id, color) => {
+    setClickedFlagColors((prev) => ({
+      ...prev,
+      [id]: color,
+    }));
   };
 
   return (
     <div className="flex justify-center">
       <div className="w-[900px] h-[600px] overflow-hidden border-b-2 border-r-2">
         <div className="grid grid-cols-6 grid-rows-6 w-full h-full">
-          {allCountries.map((country) => (
+          {gridCountries.map((id) => (
             <div
-              key={country.id}
-              className="bg-blue-200 w-full h-full border-t-2 border-l-2"
-              onClick={() => flagClick(country)}
+              key={id}
+              className={`w-full h-full border-t-2 border-l-2 ${
+                clickedFlagColors[id] === "green"
+                  ? "bg-green-300"
+                  : clickedFlagColors[id] === "red"
+                  ? "bg-red-300"
+                  : "bg-blue-300"
+              }`}
+              onClick={() => flagClick(id)}
             >
-              {country.name}
+              {allCountries[id]?.name || "Unknown Country"}
             </div>
           ))}
         </div>
