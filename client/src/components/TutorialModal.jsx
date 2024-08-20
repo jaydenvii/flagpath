9cc027968d39fef117ad7fbb20748f1960466d43
jaydenvii <jaydenyz456@gmail.com>
2024-08-20 14:38:28 -0400

    Added 'how to play' and 'borders' to the tutorial modal

--- client/src/components/TutorialModal.jsx
+++ client/src/components/TutorialModal.jsx
@@ -5,7 +5,7 @@ const TutorialModal = ({ isOpen, onClose }) => {
 
   return (
     <div className="bg-gray-800 fixed inset-0 bg-opacity-70 flex items-center justify-center">
-      <div className="bg-gray-900 p-8 rounded-lg relative border-2 border-gray-200">
+      <div className="w-[600px] bg-gray-900 p-8 rounded-lg relative border-2 border-gray-200">
         {/* Close button */}
         <button
           onClick={onClose}
@@ -14,7 +14,20 @@ const TutorialModal = ({ isOpen, onClose }) => {
           &times;
         </button>
         {/* Modal Content */}
-        <h2 className="text-3xl font-bold mb-4">Tutorial</h2>
+        <h2 className="text-3xl font-bold mb-4 text-center">How To Play</h2>
+        <p className="text-lg mb-4">
+          Click on flags to get from the
+          <span className="text-blue-300"> first country</span> to the
+          <span className="text-red-300"> last country</span>.
+        </p>
+        <p className="text-lg mb-4">
+          After correctly guessing the first flag, you can only click on flags
+          that border the current country in the real world.
+        </p>
+        <h2 className="text-3xl font-bold mb-4 text-center">Borders</h2>
+        <p className="text-lg">
+          All borders count. This means territories, tunnels, and bridges.
+        </p>
       </div>
     </div>
   );
