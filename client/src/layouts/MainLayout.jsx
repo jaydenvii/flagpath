e034df7cc0bc55afb63b6e2ed752c7c47434940e
jaydenvii <jaydenyz456@gmail.com>
2024-08-26 13:20:33 -0400

    Installed Vercal Analytics

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 const MainLayout = () => {
   return (
     <div className="bg-gray-900 text-gray-100 min-h-screen font-semibold">
       <Outlet />
+      <Analytics />
     </div>
   );
 };
